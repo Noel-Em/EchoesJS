1cf170f2735e19aea87247975d10feb91b210e6a
Noel <leonardoulino2005@gmail.com>
2024-07-31 20:12:43 +0200

    Echoes initial commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# EchoesJS
+# Echoes | JavaScript
+Echoes is a powerful and versatile JavaScript library designed to help you easily manage your website and the interactions within it.
+
+With Echoes, you can get any useful information from any page you are creating.
+The library is designed to be intuitive and easy to use, while offering a wide range of functionality to monitor and react to various browser and DOM events.
+
+Echoes has it's own [website](https://echoes.leonardoulino.com), where you can find the [documentation](https://echoes.leonardoulino.com/docs), [examples](https://echoes.leonardoulino.com/examples) and more.
+
+### Repository Info
+
+![GitHub Release](https://img.shields.io/github/v/release/Noel-Em/EchoesJS) ![GitHub License](https://img.shields.io/github/license/Noel-Em/EchoesJS)
+![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/Noel-Em/EchoesJS/total)
+## Template
+
+    import  *  as ECHOES from  './echoes.min.js';
+    
+    const  handler  =  new ECHOES.EventHandler();
+    
+    handler.onStart(() => {
+	    // Write everything that happens when you open the website
+    });
+    
+    handler.onUpdate(() => {
+	    // Write everything that needs to happen in the loop
+    });
+
+
+## Events
+
+Echoes inserts the **event** structure into JavaScript.
+Events can be of any type, from changing the mouse position to scrolling the page. 
+Although it is very reductive to speak only of these events.
+
+It is also possible to create **completely customized events** that depend on the user's conditions.
+
+There are multiple types of Events:
+ - OnHover
+ - OnClick
+ - OnBackgroundColor
+ - OnBackgroundColorChange
+ - OnTextColorChange
+ - OnTextColor
+ - MousePos
+ - ScrollPos
+ - TabInfo
+ - PageInfo
+ - WebInfo
+ 
+
+Each of these event have multiple functions that can be called.
+
+## Example
+
+    import * as ECHOES from './echoes.min.js';
+
+    const handler = new ECHOES.EventHandler();
+
+    handler.onStart(() => {
+	    ECHOES.addEvent('PositionChanged', new ECHOES.MousePos());
+    });
+    
+    handler.onUpdate(() => {
+	    if(ECHOES.getEvent('PositionChanged').changed()) {
+		    console.log('The mouse position has changed');
+	    }
+    });
---
+++ echoes.js
@@ -0,0 +1,492 @@
+"use strict";
+var PREFIX = "ECHOES";
+var DEBUG = false;
+var EVENTS = {};
+
+var SCROLL_EVENT = null;
+var SCROLL_VALUE = [0, 0];
+
+//  -----------  LOG SECTION  -----------
+
+class ELog {
+    log(message) {
+        if(DEBUG)
+            console.log(PREFIX + ": " + message)
+    }
+
+    warn(message) {
+        if(DEBUG)
+            console.warn(PREFIX + ": " + message)
+    }
+
+    error(message) {
+        if(DEBUG)
+            console.error(PREFIX + ": " + message)
+    }
+}
+
+const ELogger = new ELog();
+
+//  -----------  FUNCTIONS  -----------
+
+export function setDebug(state) {
+    if(typeof state !== 'boolean') {
+        console.error('The state must be a boolean');
+        return;
+    } else {
+        DEBUG = state;
+    }
+}
+
+export function addEvent(eventName, eventType) {
+    if (typeof eventName !== 'string') {
+        ELogger.error("Invalid event name");
+        return;
+    }
+
+    if (eventType === undefined) {
+        console.error("Invalid event type");
+        return;
+    }
+
+    if (EVENTS[eventName]) {
+        ELogger.error("The event \"" + eventName + "\" already exists");
+    } else {
+        ELogger.log("The event \"" + eventName + "\" has been created");
+        EVENTS[eventName] = eventType;
+    }
+}
+
+export function checkEvent(eventName) {
+    if(!EVENTS[eventName]) {
+        ELogger.error("The event \"" + eventName + "\" doesn't exist");
+    } else {
+        return EVENTS[eventName].check();
+    }
+}
+
+export function getEvent(eventName) {
+    if(!EVENTS[eventName]) {
+        ELogger.error("The event \"" + eventName + "\" doesn't exist");
+    } else {
+        return EVENTS[eventName];
+    }
+}
+
+export function addClass(element, className) {
+    if(document.body.contains(element)) {
+        return element.classList.add(className);
+    }
+
+    ELogger.error("There is no element " +  element);
+}
+
+export function hasClass(element, className) {
+    if(document.body.contains(element)) {
+        return element.classList.contains(className);
+    }
+
+    ELogger.error("There is no element " +  element);
+}
+
+export function removeClass(element, className) {
+    if(document.body.contains(element)) {
+        return element.classList.remove(className);
+    }
+
+    ELogger.error("There is no element " +  element);
+}
+
+
+//  ----------- EVENT SECTION -----------
+
+export class EventHandler {
+
+    constructor() {
+        ELogger.log('is now operative');
+    }
+
+    onStart(start_function) {
+        if(typeof(start_function) === 'function') {
+            start_function();
+        } else {
+            ELogger.error('The argument of "onStart" is not a function');
+        }
+    }
+
+    onUpdate(update_function, fps = 60) {
+        if (typeof(update_function) === 'function') {
+            let lastTime = 0;
+            const interval = 1000 / fps;
+    
+            function loop(currentTime) {
+                if (currentTime - lastTime >= interval) {
+                    update_function();
+                    lastTime = currentTime;
+                }
+                requestAnimationFrame(loop);
+            }
+    
+            requestAnimationFrame(loop);
+        } else {
+            ELogger.error('The argument of "onUpdate" is not a function');
+        }
+    }
+}
+
+class EventBehaviour {
+    check() {
+        throw new Error("Method 'check()' must be implemented.");
+    }
+}
+
+//  -----------  EVENTS  -----------
+
+
+//element.addEventListener('click', clickHandler);
+export class OnHover extends EventBehaviour {
+    
+    #element = null;
+
+    constructor(DOMElement) {
+        super();
+        if(document.body.contains(DOMElement)) {
+            this.#element = DOMElement;
+        }
+    }
+
+    check() {
+        return this.#element.matches(":hover");
+    }
+}
+
+export class OnClick extends EventBehaviour {
+    
+    #clicked = null;
+
+    constructor(DOMElement) {
+        super();
+        if(document.body.contains(DOMElement)) {
+            DOMElement.addEventListener('click', () => {
+                this.#clicked = true;
+            });
+        }
+    }
+
+    check() {
+        if(!this.#clicked)
+            return false;
+        this.#clicked = false;
+        return true;
+    }
+}
+
+export class OnBackgroundColorChange extends EventBehaviour {
+
+    #element = null;
+    #previousColor = null;
+
+    constructor(DOMElement) {
+        super();
+        this.#element = document.body.contains(DOMElement) ? DOMElement : null;
+        this.#previousColor = this.#element ? this.#element.style.backgroundColor : null;
+    }
+
+    check() {
+        if (this.element) {
+            const currentColor = this.element.style.backgroundColor;
+            if (currentColor !== this.#previousColor) {
+                this.#previousColor = currentColor;
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+export class OnBackgroundColor extends EventBehaviour {
+
+    #element = null;
+    #checkedColor = null;
+
+    constructor(DOMElement, colorValue) {
+        super();
+        this.#element = document.body.contains(DOMElement) ? DOMElement : null;
+        this.#checkedColor = colorValue;
+    }
+
+    check() {
+        if (this.element) {
+            return this.#element.style.backgroundColor === this.#checkedColor;
+        }
+        return false;
+    }
+}
+
+export class OnTextColorChanged extends EventBehaviour {
+
+    #element = null;
+    #previousColor = null;
+
+    constructor(DOMElement) {
+        super();
+        this.#element = document.body.contains(DOMElement) ? DOMElement : null;
+        this.#previousColor = this.#element ? this.#element.style.color : null;
+    }
+
+    check() {
+        if (this.element) {
+            const currentColor = this.element.style.color;
+            if (currentColor !== this.#previousColor) {
+                this.#previousColor = currentColor;
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+export class OnTextColor extends EventBehaviour {
+
+    #element = null;
+    #checkedColor = null;
+
+    constructor(DOMElement, colorValue) {
+        super();
+        this.#element = document.body.contains(DOMElement) ? DOMElement : null;
+        this.#checkedColor = colorValue;
+    }
+
+    check() {
+        if (this.element) {
+            return this.#element.style.color === this.#checkedColor;
+        }
+        return false;
+    }
+}
+
+export class MousePos extends EventBehaviour {
+    #lastPos = [];
+    #currentPos = [];
+
+    constructor() {
+        super();
+        this.currentPos = [-1, -1];
+        document.body.addEventListener("mousemove", (e) => {
+            this.#currentPos = [e.clientX, e.clientY];
+        });
+    }
+
+    check() {
+        return this.getPos();
+    }
+
+    changed() {
+        if (this.#currentPos[0] === this.#lastPos[0] && this.#currentPos[1] === this.#lastPos[1]) return false;
+
+        this.#lastPos = this.#currentPos;
+        return this.#currentPos;
+    }
+
+    getX() {
+        return this.#currentPos[0];
+    }
+
+    getY() {
+        return this.#currentPos[1];
+    }
+
+    getPos() {
+        return this.#currentPos;
+    }
+}
+
+function addScrollEvent() {
+    if (SCROLL_EVENT === null) {
+        SCROLL_EVENT = document.addEventListener("scroll", (e) => {
+            SCROLL_VALUE = [
+                document.documentElement.scrollTop || document.body.scrollTop,
+                document.documentElement.scrollLeft || document.body.scrollLeft
+            ];
+        });
+    }
+}
+
+export class ScrollPos extends EventBehaviour {
+
+    #lastScroll = [];
+
+    constructor() {
+        super();
+        addScrollEvent();
+        this.#lastScroll = SCROLL_VALUE;
+    }
+
+    check() {
+        return this.changed();
+    }
+
+    changed() {
+        if(SCROLL_VALUE[0] != this.#lastScroll[0] || SCROLL_VALUE[1] != this.#lastScroll[1]) {
+            this.#lastScroll = SCROLL_VALUE;
+            return this.#lastScroll;
+        }
+
+        return false;
+    }
+
+    getScrollX() {
+        return SCROLL_VALUE[1];
+    }
+
+    getScrollY() {
+        return SCROLL_VALUE[0];
+    }
+
+    getScroll() {
+        return SCROLL_VALUE;
+    }
+}
+
+export class TabInfo extends EventBehaviour {
+
+    #hasFocus = true;
+
+    constructor() {
+        super();
+        window.onfocus = (e) => {
+            this.#hasFocus = true;
+        };
+        
+        window.onblur = (e) => {
+            this.#hasFocus = false;
+        };
+    }
+
+    check() {
+        return this.#hasFocus;
+    }
+}
+
+export class PageInfo extends EventBehaviour {
+    constructor() {
+        super();
+    }
+
+    getSize() {
+
+        const width = Math.max(
+            document.body.scrollWidth,
+            document.documentElement.scrollWidth,
+            document.body.offsetWidth,
+            document.documentElement.offsetWidth,
+            document.body.clientWidth,
+            document.documentElement.clientWidth
+        );
+
+        const height = Math.max(
+            document.body.scrollHeight,
+            document.documentElement.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.offsetHeight,
+            document.body.clientHeight,
+            document.documentElement.clientHeight
+        );
+
+        return [width, height];
+
+    }
+
+    width() {
+        return this.getSize[0];
+    }
+
+    height() {
+        return this.getSize[1];
+    }
+}
+
+export class WebInfo extends EventBehaviour {
+
+    constructor() {
+        super();
+    }
+
+    check() {
+        return {
+            "browser" : this.browser(),
+            "date": this.dateTime()
+        };
+    }
+
+    browser() {
+        const userAgent = navigator.userAgent;
+
+        if (userAgent.indexOf('Chrome') > -1) {
+            if (userAgent.indexOf('Edg') > -1) {
+                return 'Microsoft Edge';
+            }
+            if (userAgent.indexOf('OPR') > -1) {
+                return 'Opera';
+            }
+            return 'Chrome';
+        }
+
+        if (userAgent.indexOf('Firefox') > -1) {
+            return 'Firefox';
+        }
+
+        if (userAgent.indexOf('Safari') > -1 && userAgent.indexOf('Chrome') === -1) {
+            return 'Safari';
+        }
+
+        if (userAgent.indexOf('MSIE') > -1 || userAgent.indexOf('Trident') > -1) {
+            return 'Internet Explorer';
+        }
+
+        return 'Unknown';
+    }
+
+    dateTime() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1;
+        const day = now.getDate();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+        const milliseconds = now.getMilliseconds();
+
+        return {
+            year: year,
+            month: month,
+            day: day,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+            milliseconds: milliseconds
+        };
+    }
+
+    date() {
+        const dt = this.dateTime();
+
+        return {
+            year: dt.year,
+            month: dt.month,
+            day: dt.day
+        };
+    }
+
+    time() {
+        const dt = this.dateTime();
+
+        return {
+            hour: dt.hours,
+            minutes: dt.minutes,
+            seconds: dt.seconds,
+            milliseconds: dt.milliseconds
+        };
+    }
+
+}
